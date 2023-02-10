f3e94473fca35263c6c9e65bfb38c04b932b1c4c
Scott Smyth <smythscott60@gmail.com>
2023-02-10 16:10:55 +0000

    Add Register User

--- run.py
+++ run.py
@@ -17,3 +17,27 @@ conn = mysql.connector.connect(
     port=PORT,
     database=DATABASE
     )
+cursor = conn.cursor()
+
+
+def register_user(username, password):
+
+    """
+    Registers new user on the system with username and password
+    If user already exists, messages is displayed,
+    If user doesn't exist, the user is registered on the system
+    """
+
+    cursor.execute("SELECT * FROM Users WHERE username = %s", (username,))
+    result = cursor.fetchone()
+
+    if result:
+        print("User already Exists Please Try a Different Username")
+        #add call to input function here
+    else:
+        cursor.execute("INSERT INTO Users (username, password) VALUES (%s, %s)", (username, password))
+        conn.commit()
+
+
+
+def _main_():
